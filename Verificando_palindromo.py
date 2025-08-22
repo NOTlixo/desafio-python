f4804f6f6990593ec73891ba7ce299994a357288
Felipe Castelar <felipecastelarvieira@gmail.com>
2025-08-22 09:55:19 -0300

    Update Verificando_palindromo.py

--- Verificando_palindromo.py
+++ Verificando_palindromo.py
@@ -6,6 +6,7 @@ for letra in range(len(junto)- 1, - 1, - 1):
     inverso += junto[letra]
 print(f"a frase normal é {junto} e seu inverso é {inverso}")
 if inverso == junto:
-    print("É u palindromo")
+    print("É um palindromo")
 else:
+
     print("A frase digitada não é um palindromo")
